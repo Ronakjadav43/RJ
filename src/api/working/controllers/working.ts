3bc2c18c3dd58155715e007536ecfbb327707734
Ronakjadav43 <ronakjadav.tech@gmail.com>
2025-06-10 16:52:16 +0530

    create new database

---
+++ src/api/working/controllers/working.ts
@@ -0,0 +1,7 @@
+/**
+ * working controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::working.working');
---
+++ src/api/working/routes/working.ts
@@ -0,0 +1,7 @@
+/**
+ * working router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::working.working');
